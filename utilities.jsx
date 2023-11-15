8fb17141a34a8e04cb8a980d347f96df5e30cbd3
miacsilva <miacsilvac@hotmail.com>
2023-11-15 22:25:58 +0000

    fix3

--- utilities.jsx
+++ utilities.jsx
@@ -2,7 +2,7 @@
 
 const getEndpoint = () => {
     
-    const productionEndpoint = 'https://facewithnobook.cyclic.com';
+    const productionEndpoint = 'https://facewithnobook.cyclic.app';
     const localEndpoint = 'http://localhost:3001';
   
     return import.meta.env.PROD ? productionEndpoint : localEndpoint;
