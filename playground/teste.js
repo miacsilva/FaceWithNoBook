5c48cd2f053124c3f61eded5a214076fd4dd1a9e
Maria Carvalho <miacsilvac@hotmail.com>
2024-02-28 20:36:40 +0000

    typo correction

--- playground/teste.js
+++
@@ -1,7 +0,0 @@
-console.log("linha 1");
-try {
-  throw new Error("erro na linha 2");
-} catch (facada) {
-  console.log("ta tudo preso seus cabroes:", facada.message);
-}
-console.log("linha 3");
--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 const getEndpoint = () => {
   /* const productionEndpoint = "https://facewithnobook.cyclic.app"; */
-  const productionEndpoint = "https://face-with-no-book-server.vercel.app/";
+  const productionEndpoint = "https://face-with-no-book-server.vercel.app";
   const localEndpoint = "http://localhost:3001";
 
   return import.meta.env.PROD ? productionEndpoint : localEndpoint;
