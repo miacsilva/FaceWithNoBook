361d1409ef15230dd9570e37f81e7442b49e9920
Maria Carvalho <miacsilvac@hotmail.com>
2024-02-28 20:40:47 +0000

    endpoint update

--- utilities.js
+++ utilities.js
@@ -1,6 +1,5 @@
 const getEndpoint = () => {
-  /* const productionEndpoint = "https://facewithnobook.cyclic.app"; */
-  const productionEndpoint = "https://face-with-no-book-server.vercel.app";
+  const productionEndpoint = "https://facewithnobook.cyclic.app";
   const localEndpoint = "http://localhost:3001";
 
   return import.meta.env.PROD ? productionEndpoint : localEndpoint;
